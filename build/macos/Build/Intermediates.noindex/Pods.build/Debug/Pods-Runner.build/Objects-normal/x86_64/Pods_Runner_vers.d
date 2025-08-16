414c33cfc27521de9a5c55a267e73e06382459d4
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-08-16 11:17:29 +1000

    first commit

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ashutoshojha/Documents/Flutter_Projects/advance_basic/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/ashutoshojha/Documents/Flutter_Projects/advance_basic/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
