0bd74db832efe5d79993c1eaeafec91e6554814c
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-08-16 11:42:55 +1000

    gitignore added

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ashutoshojha/Documents/Flutter_Projects/advance_basic/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/ashutoshojha/Documents/Flutter_Projects/advance_basic/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
