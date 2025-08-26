febb4c804b135227af5d37db61e2bfd56deec165
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-08-26 12:18:00 +1000

    app bar added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -60,6 +60,10 @@ class _ExpenseState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Expense Tracker"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
       body: Column(
         children: [
           Text("chart"),
