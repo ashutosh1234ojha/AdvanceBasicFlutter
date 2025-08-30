a6c31de6f3af5ab130dfa062d8820689f705e812
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-08-30 18:10:18 +1000

    snackbar added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -21,9 +21,26 @@ class _ExpenseState extends State<Expenses> {
   }
 
   void _removeListData(Expense item) {
+    final expenseIndex = _registeredExpenses.indexOf(item);
     setState(() {
       _registeredExpenses.remove(item);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: Text("Expense deleted"),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, item);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   void _openAddExpenseOverlay() {
