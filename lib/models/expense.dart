49fe7da8d766fdd4b77685f4120106a4384d328e
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-08-31 23:16:29 +1000

    new values added

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -31,3 +31,22 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses =
+          allExpenses.where((expense) => expense.category == category).toList();
+
+  double getTotalExpenses() {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
