83adb10c6162b31a3c5d9513e45c9acc15dc420f
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-09-28 17:13:59 +1000

    tabs added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expense_trakcer/screens/categories.dart';
+import 'package:expense_trakcer/screens/tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,6 +23,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: CategoriesScreen());
+    return MaterialApp(theme: theme, home: TabsScreen());
   }
 }
