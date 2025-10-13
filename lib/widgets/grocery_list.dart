d9355399f132bff2005e2d100714aba95ea4ef76
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-10-13 23:45:45 +1100

    dropdown added

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,12 +1,27 @@
 import 'package:expense_trakcer/data/dummy_items.dart';
+import 'package:expense_trakcer/widgets/new_item.dart';
 import 'package:flutter/material.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Heading")),
+      appBar: AppBar(
+        title: Text("Heading"),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
+
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder: (BuildContext context, int index) {
