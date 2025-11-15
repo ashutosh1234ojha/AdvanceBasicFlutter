4d481b04d8e1027d6a6962937ce0ed98f73a83ff
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-11-16 01:30:35 +1100

    firebase setup done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:expense_trakcer/screens/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -20,7 +22,9 @@ final theme = ThemeData(
   ),
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ProviderScope(child: const MyApp()));
 }
 
