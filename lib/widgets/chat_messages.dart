fc5e6694b6efa6aa6ab0ec84e67255c16a09bc2a
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-11-18 23:41:55 +1100

    msg loading done

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,9 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("sdf"));
+    return StreamBuilder(
+      stream:
+          FirebaseFirestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: false)
+              .snapshots(),
+      builder: (context, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs!.isEmpty) {
+          return Center(
+            child: Text(
+              "No message found...",
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        }
+        if (chatSnapshots.hasError) {
+          return Center(
+            child: Text(
+              "Something went wrong...",
+              style: TextStyle(color: Colors.white),
+            ),
+          );
+        }
+
+        final loadedMessage = chatSnapshots.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessage.length,
+          itemBuilder: (ctx, index) {
+            return Text(
+              loadedMessage[index].data()['text'],
+              style: TextStyle(color: Colors.white),
+            );
+          },
+        );
+      },
+    );
   }
 }
