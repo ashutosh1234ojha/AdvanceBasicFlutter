0143c8799c0de4c31b6105731a1b74d808cb94c3
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-11-19 22:28:20 +1100

    fcm setup done

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,10 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_trakcer/widgets/message_bubble.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatMessages extends StatelessWidget {
+class ChatMessages extends StatefulWidget {
   const ChatMessages({super.key});
+
+  @override
+  State<ChatMessages> createState() => _ChatMessagesState();
+}
+
+class _ChatMessagesState extends State<ChatMessages> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    final notificationSettings = await fcm.requestPermission();
+    final token = await fcm.getToken();
+    fcm.subscribeToTopic('chat');
+    print("token= $token");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
     final authenticatedUser = FirebaseAuth.instance.currentUser!;
